c0576ae222b62be1e1926392cac897dbfecf7092
Leonardo Maciel Correia <leonardomaciel022@gmail.com>
2026-06-05 12:23:32 -0300

    codeforce

---
+++ Desafio71A.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+int main () {
+
+    char word [100];
+    int length = 0 ;
+    int rep = 0;
+
+    scanf ("%d", &rep);
+
+    for (int i = 0; i < rep; i++ ) {
+        scanf ("%s", word);
+
+        length = strlen (word);
+        
+        if (length > 10) {
+            printf ("%c%d%c\n", word [0], length - 2, word [length - 1]  );
+        }
+
+        else {
+            printf ("%s\n", word);
+        }
+        int length = 0;
+        word [0] = '\0';
+    
+    }
+
+}
---
+++ Desafioaluno.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    int turma, cont;
+    float nota, maior_nota;
+    
+    char aluno[50];
+    char maior_aluno[50];
+
+    printf("-------------------\n");
+    printf(" Escola do Leo \n");
+    printf("-------------------\n");
+
+    printf("Quantidade de alunos na turma: ");
+    scanf("%d", &turma);
+
+    cont = 0;
+    maior_nota = 0.0; 
+
+    while (cont < turma) {
+        cont++;
+        printf("Aluno %dc", cont);
+        
+        printf("Nome do aluno: ");
+        scanf ("%s", &aluno);
+
+        printf("Nota do aluno: ");
+        scanf("%f", &nota);
+
+        if (nota > maior_nota) {
+            maior_nota = nota;
+            strcpy(maior_aluno, aluno);
+        }
+    }
+
+    printf( "%s foi o melhor aluno, e sua nota foi: %f.:2f\n", maior_aluno, maior_nota );
+
+    return 0;
+}
