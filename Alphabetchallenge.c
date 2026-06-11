467de4d501cf845faa67936ff8258304170c6970
Leonardo Maciel Correia <leonardomaciel022@gmail.com>
2026-06-11 14:49:09 -0300

    Counting Alphabet

--- Alphabetchallenge.c
+++ Alphabetchallenge.c
@@ -6,13 +6,13 @@
 
         int cont;
 
-        char alfabeto[] = {' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+        char alphabet[] = {' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
 
-        printf ("Digite um numero: ");
+        printf ("Enter a number: ");
         scanf ("%d", &cont);
 
         for (int i = 1; i <= cont; i++) {
-            printf ("%c", alfabeto[i]);
+            printf ("%c", alphabet[i]);
 
 
 
