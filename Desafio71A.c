98041c2e595717338b2edcddc80626ce54f969b4
Leonardo Maciel Correia <leonardomaciel022@gmail.com>
2026-06-11 14:39:22 -0300

    Abbreviate a word

--- Desafio71A.c
+++ Desafio71A.c
@@ -10,17 +10,21 @@ int main () {
     scanf ("%d", &rep);
 
     for (int i = 0; i < rep; i++ ) {
+
         scanf ("%s", &word);
 
         length = strlen (word);
         
         if (length > 10) {
+
             printf ("%c%d%c\n", word [0], length - 2, word [length - 1]  );
         }
 
         else {
+
             printf ("%s\n", word);
         }
+
         int length = 0;
         word [0] = '\0';
     
