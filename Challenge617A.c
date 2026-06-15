57222d7d5df7c547f2fd85d12817f89f6782df8d
Leonardo Maciel Correia <leonardomaciel022@gmail.com>
2026-06-15 17:13:16 -0300

    Elephant challenge

--- Challenge617A.c
+++ Challenge617A.c
@@ -1,8 +1,6 @@
     #include <stdio.h>
-    //An elephant decided to visit his friend. It turned out that the elephant's house is located at point 0 
-    //and his friend's house is located at point x(x > 0) of the coordinate line. In one step the elephant can
-    // move 1, 2, 3, 4 or 5 positions forward. Determine,
-    // what is the minimum number of steps he need to make in order to get to his friend's house.
+    //Target: Find the minimum number of operations to reach a given integer x.
+   //Constraint: Each operation can add 1, 2, 3, 4, or 5 to your current total.
     
     int main () {
 
