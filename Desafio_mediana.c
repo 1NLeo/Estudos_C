46be555d10e32d6d9adf093b7d6406865844e3a8
Leonardo Maciel Correia <leonardomaciel022@gmail.com>
2026-06-16 21:19:53 -0300

    Median challenge with arrays

---
+++ Desafio_mediana.c
@@ -0,0 +1,51 @@
+// Digitar um valor que é a quantidade de numeros que o usuario vai digitar.
+// A segunda linha de entrada uma sequencia de numeros em ordem crescente .
+// armazenar no vetor e pegar a mediana.
+
+    #include <stdio.h>
+
+    int main () {
+
+        int quant = 0, n = 0, div;
+        float med;
+
+
+        scanf ("%d", &quant);
+        int vet[quant];
+
+        for (int i = 0; i < quant; i++) {
+
+            printf ("Digite cada numero em ordem crescente: ");
+            scanf ("%d", &n);
+
+            vet[i] = n;
+
+        }
+
+            div = quant / 2;
+
+            if (quant % 2 != 0) {
+
+                printf ("%d", vet[div]);
+            }
+
+
+            if ( quant % 2 == 0) {
+
+               
+                med = (vet[div] + vet[div - 1 ]) / 2.0 ;
+
+                printf ("%.1f", med);
+            }
+
+
+
+
+        
+        
+
+        return 0;
+    }
+
+
+    
