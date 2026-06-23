890b3e0d40b9c698a8e03c3208938555f286ab79
Leonardo Maciel Correia <leonardomaciel022@gmail.com>
2026-06-23 17:45:30 -0300

    Format specifiers

---
+++ Lesson1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main () {
+
+    printf ("Hello World!\n");
+
+    printf ("Integer number: %d \n", 10);
+
+    printf ("Float number: %f\n", 3.14159265);
+
+    printf ("Float numbers with two decimal places: %.2f \n", 3.14159265);
+
+    printf ("Alphanumeric character: %c\n", 'a'); // single quotation marks to only one character 
+
+    printf ("Sequence of alphanumeric characters: %s\n", "Testing");
+
+
+    return 0;
+}
