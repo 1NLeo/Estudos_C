91c5932433f47614b8947d9736d1d58a785dba41
Leonardo Maciel Correia <leonardomaciel022@gmail.com>
2026-06-26 17:49:08 -0300

    do-while

--- Lesson6.c
+++ Lesson6.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 
+
+// "do-while" the condition is only tested in the end. 
+// The command block is executed at least once.
 int main () {
 
     int i = 1;
 
-    while (i <= 10) {
+    do {
         printf ("%d\n", i);
         i++;
-    }
+    } while (i <= 10);
 
 
     return 0;
