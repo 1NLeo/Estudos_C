4babd59e0751d574acc6426136ec28eb71d33966
Leonardo Maciel Correia <leonardomaciel022@gmail.com>
2026-06-30 18:35:02 -0300

    strcspn (finds a letter) and strcmp (compare strings)

--- Lesson15.c
+++ Lesson15.c
@@ -14,9 +14,10 @@ int main (){
         printf ("Enter a text: ");
         fgets (usrpass, 49, stdin );
 
-        usrpass[strcspn(usrpass, "\n")] = '\0';  // removes the \n
+        usrpass[strcspn(usrpass, "\n")] = '\0';  // removes the \n strcspn (string complement span) is used to find the 
+        //position of the first character in a string that belongs to a set of characters you specify.
 
-        res = strcmp(hardtxt, usrpass);
+        res = strcmp(hardtxt, usrpass); // used to compare strings (it returns 0 when true) its the == of the strings
 
         if (res == 0) {
             printf ("The text is the same.\n");
