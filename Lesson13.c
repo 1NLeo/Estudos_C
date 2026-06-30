2039f9c05fcd1ca4b22f755e72de08335250bc3b
Leonardo Maciel Correia <leonardomaciel022@gmail.com>
2026-06-30 17:20:59 -0300

    String.h (strcat)

---
+++ Lesson13.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <locale.h>
+
+int main (){
+
+    char s1 [20];
+    char s2 [20];
+
+    printf ("Enter a word: ");
+    scanf ("%s", s1);
+
+    printf ("Enter a word: ");
+    scanf ("%s", s2);
+
+    printf ("Before the strcat: \n");
+    printf ("%s\t", s1 );
+    printf ("%s\n", s2);
+
+    strcat(s1,s2); // concatenate (puts together) two strings.
+
+    printf (s1);
+
+    return 0;
+}
