c0189b33be160f845263671cb113ed7b999818a1
Leonardo Maciel Correia <leonardomaciel022@gmail.com>
2026-07-04 23:11:47 -0300

    Comparison of Numbers

---
+++ Numbers_comparison.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main () {
+
+    float x, y;
+
+    printf ("Enter the first number: ");
+    scanf ("%f", &x);
+
+    printf ("Enter the second number: ");
+    scanf ("%f", &y);
+
+    if (x > y) {
+        printf ("%.2f, is bigger than %.2f", x, y);
+    }
+
+    else if (y >x) {
+        printf("%.2f is bigger than %.2f", y, x);
+    }
+
+    return 0;
+}
