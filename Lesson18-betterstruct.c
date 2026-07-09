f85d6211ac2d58e57730f8455e6e44577a602431
Leonardo Maciel Correia <leonardomaciel022@gmail.com>
2026-07-09 15:51:33 -0300

    Better way to use structures

--- Lesson18-betterstruct.c
+++ Lesson18-betterstruct.c
@@ -15,28 +15,28 @@ typedef struct type_person type_person;
 
 int main  (){
 
-    type_person array[size];
+    type_person person[size];
     
     for (int i = 0; i < size; i++){
 
         printf ("Enter your name: ");
-        fgets(array[i].name, 49, stdin); // this different scanf is used to read spaces
-        array[i].name[strcspn(array[i].name, "\n")] = '\0'; // overwrites the \n with the `\0` "null"
+        fgets(person[i].name, 49, stdin); // this different scanf is used to read spaces
+        person[i].name[strcspn(person[i].name, "\n")] = '\0'; // overwrites the \n with the `\0` "null"
 
         printf("Enter your age: ");
-        scanf ("%d", &array[i].age);
+        scanf ("%d", &person[i].age);
 
         printf("Enter your weight: ");
-        scanf("%f", &array[i].weight);
+        scanf("%f", &person[i].weight);
         while (getchar() != '\n'); // reads character one by one until finds the \n so it doesnt brake the program
     }
 
         for (int i = 0; i < size; i++){
            
             printf ("\n");
-            printf ("Name: %s\t", array[i].name);
-            printf ("Age: %d\t", array[i].age);
-            printf ("weight: %.2f\t", array[i].weight);
+            printf ("Name: %s\t", person[i].name);
+            printf ("Age: %d\t", person[i].age);
+            printf ("weight: %.2f\t", person[i].weight);
             
         }
         
