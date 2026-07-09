e17b30c296611dbfcf9bac930f07249d3eb43828
Leonardo Maciel Correia <leonardomaciel022@gmail.com>
2026-07-08 22:48:05 -0300

    Way too long words

--- Challenge71A.c
+++ Challenge71A.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 #include <string.h>
-//way too long words
+
+//Challenge Summary
+//We need to process a list of words and abbreviate any word that is strictly longer than 10 characters.
+// The abbreviation format is: [First Letter] + [Number of skipped middle letters] + [Last Letter].
+// Words that have 10 characters or fewer must remain completely unchanged.
+
+//Input
+//The first line contains an integer n (1 ≤ n ≤ 100). Each of the following n lines contains one word. 
+//All the words consist of lowercase Latin letters and possess the lengths of from 1 to 100 characters.
+
+//Output
+//Print n lines. The i-th line should contain the result of replacing of the i-th word from the input data.
+
 int main () {
 
     char word [100];
