8dd2c33db58b4380971dd87b65c84b4f2c7eddfd
Leonardo Maciel Correia <leonardomaciel022@gmail.com>
2026-07-08 22:52:32 -0300

    Domino piling

--- Challenge50A.c
+++ Challenge50A.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 
-//Challenge Summary
-//We need to calculate the maximum number
-//of $2 \times 1$ dominoes that can fit onto an $M \times N$ rectangular grid without overlapping and without extending past the 
-//boundaries of the board. The dominoes can be rotated horizontally or vertically.
+// Challenge Summary
+// We need to calculate the maximum number
+// of $2 \times 1$ dominoes that can fit onto an $M \times N$ rectangular grid without overlapping and without extending past the 
+// boundaries of the board. The dominoes can be rotated horizontally or vertically.
+
+// Input
+// In a single line you are given two integers M and N — board sizes in squares (1 ≤ M ≤ N ≤ 16).
+
+// Output
+// Output one number — the maximal number of dominoes, which can be placed.
 
 int main () {
 
