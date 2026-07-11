34ab051fec934768d8a9014042d9c66d335888fc
Leonardo Maciel Correia <leonardomaciel022@gmail.com>
2026-07-11 16:44:36 -0300

    Challenge 67

---
+++ Challenge67.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main () {
+
+    int num;
+    int siz;
+
+    scanf ("%d", &siz);
+
+    for (int i = 0; i < siz; i++) {
+        num = 0;
+        scanf ("%d", &num);
+        printf ("%d\n", 67);
+    }
+
+ 
+
+    return 0;
+}
