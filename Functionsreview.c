4aa343a6dfd36fc949802a1f7b951cd664048309
Leonardo Maciel Correia <leonardomaciel022@gmail.com>
2026-07-17 22:08:59 -0300

    Review functions

---
+++ Functionsreview.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void calculatesquare () {
+    
+    int number;
+    printf ("Enter a number: ");
+    scanf ("%d", &number);
+
+    int square = number * number;
+    printf ("Square of %d is %d \n", number, square);
+}
+int main () {
+
+    
+    calculatesquare();
+
+
+    return 0;
+}
