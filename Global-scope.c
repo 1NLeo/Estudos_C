1f70a8d3df224ff79d7df7799b472a3aa2c399f7
Leonardo Maciel Correia <leonardomaciel022@gmail.com>
2026-07-19 20:52:52 -0300

    Global scope

---
+++ Global-scope.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+// Global Scope. Better to use than the local scope.
+int result;
+
+void addnumbers(int number1, int number2){
+    result = number1 + number2;
+    printf ("Result = %d", result);
+}
+
+int main (){
+
+    addnumbers (6,7);
+
+    return 0;
+}
