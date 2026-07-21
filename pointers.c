4f805d7a1a9085f5ffc61f2168ceb8d5ec8e2e56
Leonardo Maciel Correia <leonardomaciel022@gmail.com>
2026-07-20 23:23:38 -0300

    Pointers

---
+++ pointers.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main () {
+
+    int age = 25;
+
+    int* ptr = &age;
+
+    printf ("Address: %p\n", ptr);
+
+    printf ("Value: %d\n", *ptr);
+
+    *ptr = 31;
+
+    printf ("%d\n", age);
+
+
+    // Example
+
+    // int main () {
+
+    // int number;
+    // int* ptr;
+
+    // ptr = number; Invalid (ptr is a pointer that can oly store memory adress, but number is not a memory adress)
+    // *ptr = &number; Invalid (ptr gives the value stored in the ptr location, however &number gives the memory adress)
+    // ptr = &number; Valid (both ptr and &number represents the memory address)
+    // *ptr = number; Valid (both ptr and &number represents the value stored in the memory location)
+    
+    // }
+    return 0;
+}
