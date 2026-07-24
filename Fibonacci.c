cb9a13d580c8c8fb9501caa9459b6d399de0a6d0
Leonardo Maciel Correia <leonardomaciel022@gmail.com>
2026-07-23 22:09:50 -0300

    Playing with Fibonacci

--- Fibonacci.c
+++ Fibonacci.c
@@ -4,10 +4,16 @@ void fib (float first_num ,float second_num) {
 
     float last_num = 0;
 
-    for (int i = 0; i < 100; i++) {
+    for (int i = 0; i < 10; i++) {
 
         last_num = first_num + second_num;
-        printf ("%.1lf \n", last_num);
+        if (i < 1){
+            printf ("%d\t", 0);
+            printf ("%d\t", 1);
+        }
+        printf ("%.0lf \t", last_num);
+
+
 
         first_num = second_num;
         second_num = last_num;
