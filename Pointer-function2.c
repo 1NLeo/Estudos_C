401325197cf6c7ce488b3b2150aa8b0cc86f26ae
Leonardo Maciel Correia <leonardomaciel022@gmail.com>
2026-07-27 23:15:14 -0300

    Pointer with function (int)

---
+++ Pointer-function2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int* addnumber (int* num1, int* num2, int* sum) {
+
+    *sum = *num1 + *num2;
+
+    return sum;
+} 
+
+int main () {
+
+    int number1 = 32;
+    int number2 = 18;
+    int sum;
+
+    int* result = addnumber (&number1, &number2, &sum);
+
+    printf ("The sum is: %d", *result);
+    return 0;
+}
