019bf1941d8f5d3a19ce0adeff2926142e2041bb
Leonardo Maciel Correia <leonardomaciel022@gmail.com>
2026-07-29 19:29:16 -0300

    Working with pointers3

---
+++ Pointer-challenge4.c
@@ -0,0 +1,26 @@
+//Escreva uma função int soma_vetor(int *v, int tamanho) 
+//que soma os elementos de um vetor de inteiros usando aritmética de ponteiros (sem usar v[i], só *(v+i)).
+#include <stdio.h>
+
+int sum_array(int *v) {
+
+    int sum = 0;
+
+    for (int i = 0; i < 5; i++) {
+        sum += *(v + i);
+    }
+
+    return sum;
+}
+
+int main () {
+
+    int arr[5] = {1, 2, 3, 4, 5};
+
+    int result = sum_array (arr);
+
+    printf ("Result: %d", result);
+
+
+    return 0;
+}
