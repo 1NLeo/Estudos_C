9c77ec6b3844ad5ec379974b8f131042be13e862
Leonardo Maciel Correia <leonardomaciel022@gmail.com>
2026-07-29 16:27:50 -0300

    Working with pointers

---
+++ Pointer-challenge2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main () {
+
+    int x = 10;
+    int* a = &x;
+    int* b = &x;
+    int* c = &x;
+
+    printf ("X value: %d\n", x); 
+    printf ("A value: %d\n", *a);
+    printf ("B value: %d\n", *b);
+    printf ("C value: %d\n", *c);
+
+    printf ("X address: %p\n", &x); // To show the adress of x we use the &
+    printf ("A address: %p\n", a); // To show the adress of the pointer we use the variable name itself 
+    printf ("B address: %p\n", b); // because they alreadry have the adress of x, use the & on the pointer would show their 
+    printf ("C address: %p", c);   // variable address itself
+
+    return 0;
+}
