f97773b88b05bf9bdca83e27da7db04df2ee32f2
Leonardo Maciel Correia <leonardomaciel022@gmail.com>
2026-08-16 14:41:48 -0300

    How the modulus works

---
+++ Modulus-operator.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main () {
+
+    int x = 19, y = 3;
+
+    int modulus = x % y;
+
+    printf ("Remainder: %d", modulus); // The result is 1 because 6 * 3 = 18 and 19 - 18 = 1, so the remainder is equal to 1. 
+
+    return 0;
+}
