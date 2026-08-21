196dceb47a7243d1cfa05723e802b84853424ba2
Leonardo Maciel Correia <leonardomaciel022@gmail.com>
2026-08-20 21:08:18 -0300

    Pointers review

--- Pointer_review.c
+++ Pointer_review.c
@@ -6,13 +6,16 @@ int main () {
     int *y = &x;
     x = 15;
     *y = 20;
+
     // The value depends of the order of the pointer and the adress
+
     printf ("%d\n", *y);
     printf ("%d\n", x);
+
     // But the pointer changes both values as well.
+    
     *y = 150;
      x = 200;
-    
 
     printf ("%d\n", *y);
     printf ("%d\n", x);
