69528f455cb8ac9c676e29077a7adaa3ab6ed1dc
Leonardo Maciel Correia <leonardomaciel022@gmail.com>
2026-09-10 22:29:15 -0300

    Data_structures

---
+++ data_structure/quick_sort.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+// Divide and conquer
+// chose one element called pivo
+// smallest  pivo  greater
+
+int particionar(int v[], int inicio, int fim) {
+
+    int pivo = v[fim];
+    int i = inicio - 1;
+
+    for (int j = inicio; j < fim; j++) {
+
+        if (v[j] < pivo) {
+            i++;
+
+            int temp = v[i];
+            v[i] = v[j];
+            v[j] = temp;
+        }
+    }
+
+    int temp = v[i + 1];
+    v[i + 1] = v[fim];
+    v[fim] = temp;
+
+    return i + 1;
+}
+
+void quickSort(int v[], int inicio, int fim) {
+
+    if (inicio < fim) {
+
+        int p = particionar(v, inicio, fim);
+
+        quickSort(v, inicio, p - 1);
+        quickSort(v, p + 1, fim);
+    }
+}
